77dcb776a58b0a99138b61903086f27e6c862809
shivam-jainn <shivam.clgstash@gmail.com>
2025-08-03 02:31:43 +0530

    init springboot

---
+++ anymoly/src/main/java/com/example/demo/AnymolyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnymolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnymolyApplication.class, args);
+	}
+
+}
